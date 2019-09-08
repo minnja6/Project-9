ce2e986b0fbff5e5ced9a86e63c3d2fb3b45ea60
minnja6 <jasmine.minniweather@verizon.com>
2019-09-08 16:23:17 -0500

    Users complete
    users works, working on the remaining requirements for courses. Delete .eslintric.js because I believe it is not nes. Will see

--- routes/courses.js
+++ routes/courses.js
@@ -2,89 +2,90 @@ const express = require('express');
 const router = express.Router();
 const { Course } = require('../models');
 
-function asyncHandler(cb) {
-    return async (req, res, next) => {
-        try {
-            await cb(req, res, next);
-        } catch (err) {
-            next(err);
-        }
-    }
-}
-// Send a GET request to courses to view all courses
-router.get('/courses', function (req, res) {
-    Course.findAll().then(courses => {
-        if (courses) {
-            res.status(200).json(courses);
-        } else {
-            res.status(404).json({ message: "Courses not found." });
-        }
-    }).catch(function (err) {
-        res.send(500);    
-});
-});
-//GET individual course by id
-router.get('/courses/:id', function(req, res, next) {
-    Course.findByPk(req.params.id).then(course => {
-      if(course) {
-        res.status(200).json(course);
-    } else {
-        res.status(404).json({ message: "Course not found." });
-      }
-    }).catch(function(err) {
-      res.send(500);
-});
-});
-//Send a POST request to /courses to CREATE a new course
-// router.post('/courses', (req, res, next) => {
-//     Course.create(req.body).then(function (course) {
-//         let { title, id, userId, description } = req.body;
-//         // redirects to home page after creating course
-//         res.redirect('/')      
-//     })
-//         .catch(err => {
-//             if (err.name === "SequelizeValidationError") {
-//                 res.json({
-//                     errors: err.errors
-//                 })
-//             } else {
-//                 throw error;
-//             }
-//         }).catch(function (error) {
-//             res.send(500, error);
-//         });
+
+// function asyncHandler(cb) {
+//     return async (req, res, next) => {
+//         try {
+//             await cb(req, res, next);
+//         } catch (err) {
+//             next(err);
+//         }
+//     }
+// }
+// // Send a GET request to courses to view all courses
+// router.get('/courses', function (req, res) {
+//     Course.findAll().then(courses => {
+//         if (courses) {
+//             res.status(200).json(courses);
+//         } else {
+//             res.status(404).json({ message: "Courses not found." });
+//         }
+//     }).catch(function (err) {
+//         res.send(500);    
 // });
-//Send a POST request to /courses to CREATE a new course
-router.post("/courses", (req, res, next) => {
-    if (req.body.title && req.body.description) {
-      const courseData = {
-        user: req.user,
-        title: req.body.title,
-        description: req.body.description,
-        estimatedTime: req.body.estimatedTime,
-        materialsNeeded: req.body.materialsNeeded
-      };
-      if (req.user) {
-        Course.create(courseData, function(error) {
-          if (error) {
-            return next(error);
-          } else {
-            res.location("/courses");
-            res.sendStatus(201);
-          }
-        });
-      } else {
-        const error = new Error("Please login to create post.");
-        error.status = 400;
-        return next(error);
-      }
-    } else {
-      const error = new Error("Title and description are required.");
-      error.status = 400;
-      return next(error);
-    }
-    console.log(req.body);
-  });
+// });
+// //GET individual course by id
+// router.get('/courses/:id', function(req, res, next) {
+//     Course.findByPk(req.params.id).then(course => {
+//       if(course) {
+//         res.status(200).json(course);
+//     } else {
+//         res.status(404).json({ message: "Course not found." });
+//       }
+//     }).catch(function(err) {
+//       res.send(500);
+// });
+// });
+// //Send a POST request to /courses to CREATE a new course
+// // router.post('/courses', (req, res, next) => {
+// //     Course.create(req.body).then(function (course) {
+// //         let { title, id, userId, description } = req.body;
+// //         // redirects to home page after creating course
+// //         res.redirect('/')      
+// //     })
+// //         .catch(err => {
+// //             if (err.name === "SequelizeValidationError") {
+// //                 res.json({
+// //                     errors: err.errors
+// //                 })
+// //             } else {
+// //                 throw error;
+// //             }
+// //         }).catch(function (error) {
+// //             res.send(500, error);
+// //         });
+// // });
+// //Send a POST request to /courses to CREATE a new course
+// router.post("/courses", (req, res, next) => {
+//     if (req.body.title && req.body.description) {
+//       const courseData = {
+//         user: req.user,
+//         title: req.body.title,
+//         description: req.body.description,
+//         estimatedTime: req.body.estimatedTime,
+//         materialsNeeded: req.body.materialsNeeded
+//       };
+//       if (req.user) {
+//         Course.create(courseData, function(error) {
+//           if (error) {
+//             return next(error);
+//           } else {
+//             res.location("/courses");
+//             res.sendStatus(201);
+//           }
+//         });
+//       } else {
+//         const error = new Error("Please login to create post.");
+//         error.status = 400;
+//         return next(error);
+//       }
+//     } else {
+//       const error = new Error("Title and description are required.");
+//       error.status = 400;
+//       return next(error);
+//     }
+//     console.log(req.body);
+//   });
 
 //Send a PUT request to /quotes/:id to UPDATE (edit) a course
 // router.put('/courses/:id', asyncHandler(async (req, res) => {
@@ -106,7 +107,5 @@ router.post("/courses", (req, res, next) => {
 //     await Users.deleteCourse(course);
 //     res.status(204).end();
 // }));
-
-
 
 module.exports = router; 
--- routes/users.js
+++ routes/users.js
@@ -1,150 +1,96 @@
 'use strict';
-
 const express = require('express');
-const { check, validationResult } = require('express-validator/check');
-const bcryptjs = require('bcryptjs');
+const router = express.Router();
 const auth = require('basic-auth');
+const bcryptjs = require('bcryptjs');
+const { User } = require('../models');
 
-// This array is used to keep track of user records
-// as they are created.
-const users = [];
+//AsyncHandler 
+function asyncHandler(cb){
+  return async (req, res, next)=>{
+    try {
+      await cb(req,res, next);
+    } catch(err){
+      err.status = 400;
+      next(err);
+    }
+  };
+}
 
-/**
- * Middleware to authenticate the request using Basic Authentication.
- * @param {Request} req - The Express Request object.
- * @param {Response} res - The Express Response object.
- * @param {Function} next - The function to call to pass execution to the next middleware.
- */
-const authenticateUser = (req, res, next) => {
+//Authentication middleware 
+const authenticateUser = async (req,res,next) => {
   let message = null;
-
-  // Get the user's credentials from the Authorization header.
   const credentials = auth(req);
-
-  if (credentials) {
-    // Look for a user whose `username` matches the credentials `name` property.
-    const user = users.find(u => u.username === credentials.name);
-
-    if (user) {
-      const authenticated = bcryptjs
-        .compareSync(credentials.pass, user.password);
-      if (authenticated) {
-        console.log(`Authentication successful for username: ${user.username}`);
-
-        // Store the user on the Request object.
-        req.currentUser = user;
-      } else {
-        message = `Authentication failure for username: ${user.username}`;
+  console.log(credentials);
+  if(credentials){
+    const user = await User.findOne({
+      where: {emailAddress: credentials.name}
+    })
+    if(user){
+      const authenticated = bcryptjs.compareSync(credentials.pass, user.password)
+      if(authenticated){
+        req.currentUser = await User.findOne({
+          where: {emailAddress: credentials.name},
+          attributes: { exclude: ['password','createdAt','updatedAt']}
+        })
+      }else{
+        message = `Authentication failure for username: ${user.emailAddress}`
       }
-    } else {
+    }else {
       message = `User not found for username: ${credentials.name}`;
     }
-  } else {
+  }else {
     message = 'Auth header not found';
   }
 
-  if (message) {
-    console.warn(message);
-    res.status(401).json({ message: 'Access Denied' });
+  if(message){
+    res.status(401).json({message: 'Access Denied: '+ message })
+  }else{
+    next()
+  }
+}
+
+// Returns the currently authenticated user
+router.get('/users',authenticateUser, asyncHandler( async (req,res) => {
+  res.json(req.currentUser);
+}));
+
+
+// Creates user, sets the Location header to "/", and returns no content
+router.post('/users', asyncHandler( async (req,res) => {
+  const errors = [];
+  const userContent = req.body
+  if(!userContent.firstName){
+    errors.push('Please provide a value for "firstName"');
+  }
+  if(!userContent.lastName){
+    errors.push('Please provide a value for "lastName"');
+  }
+  if(!userContent.emailAddress){
+    errors.push('Please provide a value for "emailAddress"');
+  }
+  if(!userContent.password){
+    errors.push('Please provide a value for "password"');
+  }
+
+  if(errors.length == 0){
+    const duplicateEmail = await User.findOne({
+      where: {emailAddress: req.body.emailAddress}
+    });
+
+    if(duplicateEmail){
+      throw new Error (req.body.emailAddress + ' is already associated with an existing account')
+    }else{
+      await User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        emailAddress: req.body.emailAddress,
+        password: bcryptjs.hashSync(req.body.password)
+      })
+      res.status(201).location('/').end();
+    }
   } else {
-    next();
+    res.status(400).json({errors})
   }
-};
-
-// Construct a router instance.
-const router = express.Router();
-
-// Route that returns the current authenticated user.
-router.get('/users', authenticateUser, (req, res) => {
-  const user = req.currentUser;
-
-  res.json({
-    name: user.name,
-    username: user.username,
-  });
-});
-
-// Route that creates a new user.
-router.post('/users', [
-  check('name')
-    .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Please provide a value for "name"'),
-  check('username')
-    .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Please provide a value for "username"'),
-  check('password')
-    .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Please provide a value for "password"'),
-], (req, res) => {
-  // Attempt to get the validation result from the Request object.
-  const errors = validationResult(req);
-
-  // If there are validation errors...
-  if (!errors.isEmpty()) {
-    // Use the Array `map()` method to get a list of error messages.
-    const errorMessages = errors.array().map(error => error.msg);
-
-    // Return the validation errors to the client.
-    return res.status(400).json({ errors: errorMessages });
-  }
-
-  // Get the user from the request body.
-  const user = req.body;
-
-  // Hash the new user's password.
-  user.password = bcryptjs.hashSync(user.password);
-  // Set the status to 201 Created and end the response.
-  return res.status(201).end();
-});
-
-module.exports = router;// var express = require('express');
-// var router = express.Router();
-// const { User } = require('../models');
-// //using the require method to import the check() and validationResult() methods from express-validator
-// const { check, validationResult } = require('express-validator');
-// //calling the check() method and passing in the field name validate
-// const nameValidator = check('name');
-// const bcryptjs = require('bcryptjs');
-
-
-
-// /*The GET/api/users route, retrieves a list of user accounts and returns it as JSON*/
-// router.get('/users', function (req, res) {
-//   User.findAll().then(users => {
-//       if (users) {
-//           res.status(200).json(users);
-//       } else {
-//           res.status(404).json({ message: "User not found." });
-//       }
-//   }).catch(function (err) {
-//       res.send(500);    
-// });
-// });
-// // Route that creates a new user.
-// router.post('/users', [
-//     check('name')
-//       .exists({ checkNull: true, checkFalsy: true })
-//       .withMessage('Please provide a value for "name"'),
-//     check('username')
-//       .exists({ checkNull: true, checkFalsy: true })
-//       .withMessage('Please provide a value for "username"'),
-//     check('password')
-//       .exists({ checkNull: true, checkFalsy: true })
-//       .withMessage('Please provide a value for "password"'),
-//   ], (req, res) => {
-//     // Attempt to get the validation result from the Request object.
-//     const errors = validationResult(req);
-//     // If there are validation errors ->
-//     if (!errors.isEmpty()) {
-//       // Use the Array `map()` method to get a list of error messages.
-//       const errorMessages = errors.array().map(error => error.msg);
-//       // Return the validation errors to the client.
-//       return res.status(400).json({ errors: errorMessages });
-//     }
-//     // Hash the new user's password.
-//     user.password = bcryptjs.hashSync(user.password);
-//     // Set the status to 201 Created and end the response.
-//     return res.status(201).end();
-//   });
-// module.exports = router;
-
+}));
+module.exports = router;
